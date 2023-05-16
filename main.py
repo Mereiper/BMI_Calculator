e809dd35021085a1e00b2a5712b4d82497e5968f
Emre Iper <mereiper789@gmail.com>
2023-05-16 21:27:18 +0300

    Interface added

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import tkinter
 import setup
 
+# setups
 screen = tkinter.Tk()
 setup.setup_win(screen)
+
+label = tkinter.Label()
+label2 = tkinter.Label()
+setup.setup_label(label=label, label2=label2)
+
+entry = tkinter.Entry()
+entry2 = tkinter.Entry()
+setup.setup_entry(entry=entry, entry2=entry2)
+
+setup.setup_placement(label=label, label2=label2, entry=entry, entry2=entry2)
 
 screen.mainloop()
--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-import tkinter
-
-
 def setup_win(screen):
     screen.title("BMI Calculator")
     screen.eval('tk::PlaceWindow . center')
     screen.minsize(width=200, height=200)
-    screen.maxsize(width=400, height=400)
+    screen.maxsize(width=300, height=300)
+
+
+def setup_label(label, label2):
+    label.config(text="Enter your weight (kg)", padx=10, pady=10)
+    label2.config(text="Enter your height (cm)", padx=10, pady=10)
+
+
+def setup_entry(entry, entry2):
+    entry.config(width=10)
+    entry2.config(width=10)
+
+
+def setup_placement(label, label2, entry, entry2):
+    label.pack()
+    entry.pack()
+    label2.pack()
+    entry2.pack()
